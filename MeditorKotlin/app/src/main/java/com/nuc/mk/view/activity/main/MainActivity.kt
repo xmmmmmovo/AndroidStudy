0f13b40587917116612ad3c8a4f673c896d8bc16
xmmmmmovo <13256172360@163.com>
2020-02-15 01:10:59 +0800

    main_succ

--- MeditorKotlin/app/src/main/java/com/nuc/mk/view/activity/main/MainActivity.kt
+++ MeditorKotlin/app/src/main/java/com/nuc/mk/view/activity/main/MainActivity.kt
@@ -2,12 +2,41 @@ package com.nuc.mk.view.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.nuc.mk.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val navController =
+            findNavController(R.id.main_nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.filesFragment, R.id.settingsFragment
+            ), drawer_layout
+        )
+        setupActionBarWithNavController(
+            navController,
+            appBarConfiguration
+        )
+        main_nav_view.setupWithNavController(
+            navController
+        )
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return super.onSupportNavigateUp() ||
+                findNavController(R.id.main_nav_host_fragment)
+                    .navigateUp(appBarConfiguration)
+
     }
 }
